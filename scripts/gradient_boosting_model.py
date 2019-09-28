fda362452a8b3d5d9b406ca032fae613c06de235
bruno <bfl2@cin.ufpe.br>
2019-09-28 12:52:48 -0300

    Properly balancing train dataset

--- scripts/gradient_boosting_model.py
+++ scripts/gradient_boosting_model.py
@@ -10,24 +10,28 @@ def main():
     print("Running Gradient Boosting model")
 
     dt = bdt.getOneHotEncodedDataset(remove_extra_classes=False)
-    #dt = bdt.getBalancedDataset(dt)
 
     X  = dt.iloc[ : , 1:31 ]
     y = dt.iloc[ : , 31 ]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=100, stratify=y)
-    gb_clf = GradientBoostingClassifier(n_estimators=100, learning_rate=0.5, max_depth=3, random_state=100)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42, stratify=y)
+
+    train = bdt.getBalancedDataset(X_train, y_train)
+    X_train  = train.iloc[ : , :-1 ]
+    y_train = train.iloc[ : , -1]
+
+    gb_clf = GradientBoostingClassifier(n_estimators=100, learning_rate=0.5, max_depth=3, random_state=42)
     gb_clf.fit(X_train, y_train)
 
-    scores = cross_val_score(gb_clf, X_test, y_test, cv=5, scoring='accuracy')
-    print("Score: {0:.3f} +-({1:.3f}))".format(scores.mean(), scores.std()))
+    metric = 'accuracy'
+    scores = cross_val_score(gb_clf, X_test, y_test, cv=5, scoring=metric)
 
     y_pred = gb_clf.predict(X_test)
     conf_matrix = confusion_matrix(y_test, y_pred)
     tn, fp, fn, tp = conf_matrix.ravel()
 
-    print('Accuracy testing : {:.3f} (+-{:.3f})'.format(scores.mean(), scores.std()))
-    print("confusion matrix:", conf_matrix)
+    print('{} testing : {:.3f} (+-{:.3f})'.format(metric, scores.mean(), scores.std()))
+    print("confusion matrix:\n", conf_matrix)
     print("True Negative:{0}, False Positive:{1} \nFalse Negative:{2}, True Positive:{3}".format(tn, fp, fn, tp))
 
     print("Running Gradient Boosting  model")
--- scripts/mlp_model.py
+++ scripts/mlp_model.py
@@ -7,25 +7,27 @@ from sklearn.metrics import classification_report, confusion_matrix
 
 def main():
     print("Running MLP model")
-    dt = bdt.getOneHotEncodedDataset(remove_extra_classes=False)
-    #dt = bdt.getBalancedDataset(dt)
+    dt = bdt.getOneHotEncodedDataset(remove_extra_classes=True)
 
     X  = dt.iloc[ : , 1:31 ]
     y = dt.iloc[ : , 31 ]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=100, stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42, stratify=y)
+
+    train = bdt.getBalancedDataset(X_train, y_train)
+    X_train  = train.iloc[ : , :-1 ]
+    y_train = train.iloc[ : , -1]
 
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(90, 40), random_state=1)
     clf.fit(X_train, y_train)
 
-    scores = cross_val_score(clf, X_train, y_train, cv=5, scoring='accuracy')
-
-    print('Accuracy testing : {:.3f} (+-{:.3f})'.format(scores.mean(), scores.std()))
+    metric = 'accuracy'
+    scores = cross_val_score(clf, X_test, y_test, cv=5, scoring=metric)
 
     y_pred = clf.predict(X_test)
     conf_matrix = confusion_matrix(y_test, y_pred)
     tn, fp, fn, tp = conf_matrix.ravel()
 
-    print('Accuracy testing : {:.3f} (+-{:.3f})'.format(scores.mean(), scores.std()))
+    print('{} testing : {:.3f} (+-{:.3f})'.format(metric, scores.mean(), scores.std()))
     print("confusion matrix:", conf_matrix)
     print("True Negative:{0}, False Positive:{1} \nFalse Negative:{2}, True Positive:{3}".format(tn, fp, fn, tp))
 
